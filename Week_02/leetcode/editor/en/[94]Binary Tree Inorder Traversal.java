7f4c496d281324712b4ff4176366256c8f466fb3
Zhang Jun <zj_jun@163.com>
2020-11-10 03:11:30 +0800

    完成[94]二叉树中序遍历

--- Week_02/leetcode/editor/en/[94]Binary Tree Inorder Traversal.java
+++ Week_02/leetcode/editor/en/[94]Binary Tree Inorder Traversal.java
@@ -1,62 +1,66 @@
 //Given the root of a binary tree, return the inorder traversal of its nodes' va
-//lues. 
+//lues.
 //
-// 
-// Example 1: 
 //
-// 
+// Example 1:
+//
+//
 //Input: root = [1,null,2,3]
 //Output: [1,3,2]
-// 
 //
-// Example 2: 
 //
-// 
+// Example 2:
+//
+//
 //Input: root = []
 //Output: []
-// 
 //
-// Example 3: 
 //
-// 
+// Example 3:
+//
+//
 //Input: root = [1]
 //Output: [1]
-// 
 //
-// Example 4: 
 //
-// 
+// Example 4:
+//
+//
 //Input: root = [1,2]
 //Output: [2,1]
-// 
 //
-// Example 5: 
 //
-// 
+// Example 5:
+//
+//
 //Input: root = [1,null,2]
 //Output: [1,2]
-// 
 //
-// 
-// Constraints: 
 //
-// 
-// The number of nodes in the tree is in the range [0, 100]. 
-// -100 <= Node.val <= 100 
-// 
 //
-// 
+// Constraints:
 //
-// Follow up: 
 //
-// Recursive solution is trivial, could you do it iteratively? 
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
 //
-// 
-// Related Topics Hash Table Stack Tree 
+//
+//
+//
+// Follow up:
+//
+// Recursive solution is trivial, could you do it iteratively?
+//
+//
+// Related Topics Hash Table Stack Tree
 // 👍 3834 👎 166
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -73,8 +77,16 @@
  * }
  */
 class Solution {
+    List<Integer> list = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        
+        if (root != null) {
+            list = inorderTraversal(root.left);
+            list.add(root.val);
+            list = inorderTraversal(root.right);
+        }
+        return list;
     }
+
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
