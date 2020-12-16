ca77833e57972c8e02b25da131541faab16b29f3
ZJ <zj_jun@163.com>
2020-12-17 06:05:21 +0800

    完成[249]N 叉树的层序遍历

--- Week_02/leetcode/editor/en/[429]N-ary Tree Level Order Traversal.java
+++ Week_02/leetcode/editor/en/[429]N-ary Tree Level Order Traversal.java
@@ -54,9 +54,27 @@ class Node {
 };
 */
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
-        
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                level.add(node.val);
+                queue.addAll(node.children);
+            }
+            result.add(level);
+        }
+        return result;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
